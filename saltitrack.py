18b6686d608c919bc6e6d71b2de174d022c44675
enzoperesafonso <enzo.peres.afonso@gmail.com>
2023-08-31 09:49:38 +0200

    updated definitions

--- saltitrack.py
+++ saltitrack.py
@@ -3,4 +3,30 @@ from astropy import coordinates
 
 
 def get_max_track_time(ob : coordinates.HADec):
-    return 0
+    """Determines the maximum track length for a target with the specified declination.
+    The track length is given as a Universal Time (rather than sidereal time)
+    difference. In other words, it is the time difference you measure with a normal
+    clock.
+
+    Parameters
+    ----------
+    declination : float
+        Declination of target (in degrees).
+
+
+    Raises
+    ------
+    OutOfVisibilityRangeException
+        Raised if the target with the given
+        declination never enters the visibility zone.
+
+    Returns
+    -------
+    max_track_length : float
+        The maximum tracklength (in seconds).
+    """
+    pass
+
+
+def is_visible_from_salt():
+    pass
